fdac07ee5adb72d80c7f0c016b7c998a2804c4d9
sachinyadav1234sysy <sachinyadav1234sysy@gmail.com>
2026-09-17 21:20:22 +0530

    message

--- src/app.js
+++ src/app.js
@@ -1,31 +1,37 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
+const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-const app = express()
+// Routes
+const authRouter = require("./routes/auth.routes");
+const accountRouter = require("./routes/account.routes");
+const transactionRoutes = require("./routes/transaction.routes");
 
-
-app.use(express.json())
-app.use(cookieParser())
-
-/**
- * - Routes required
- */
-const authRouter = require("./routes/auth.routes")
-const accountRouter = require("./routes/account.routes")
-const transactionRoutes = require("./routes/transaction.routes")
-
-/**
- * - Use Routes
- */
-
+// Home Route
 app.get("/", (req, res) => {
-    res.send("Ledger Service is up and running")
-})
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.use("/api/auth", authRouter)
-app.use("/api/accounts", accountRouter)
-app.use("/api/transactions", transactionRoutes)
+// API Routes
+app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
+app.use("/api/transactions", transactionRoutes);
 
-module.exports = app
+// Global Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error("Unhandled error:", err);
+
+    res.status(err.status || 500).json({
+        status: "error",
+        message: err.message || "Internal Server Error"
+    });
+});
+
+module.exports = app;
